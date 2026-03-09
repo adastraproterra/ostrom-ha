11621ce057aa32760b042b9fb8bd9230101e7c5d
adastraproterra <22058531+adastraproterra@users.noreply.github.com>
2026-03-09 19:55:30 +0100

    Add constants for Ostrom integration
    Define constants for the Ostrom integration.

---
+++ custom_components/ostrom/const.py
@@ -0,0 +1,14 @@
+"""Constants for the Ostrom integration."""
+
+DOMAIN = "ostrom"
+
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
+CONF_ZIP_CODE = "zip_code"
+CONF_ARBEITSPREIS = "arbeitspreis"
+
+URI_AUTH = "https://auth.production.ostrom-api.io/oauth2/token"
+URI_API = "https://production.ostrom-api.io"
+
+UPDATE_INTERVAL_MINUTES = 60
+
